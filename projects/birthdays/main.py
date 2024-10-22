5669e4266ee3e470c096e91cf78e92425bce9fcb
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-10-22 18:46:38 -0500

    Add files via upload

--- projects/birthdays/main.py
+++ projects/birthdays/main.py
@@ -1,1 +1,31 @@
+#Print "Welcome" message
 
+print("Welcome to the Birthday Registrator!")
+
+#Ask for birthdays and names
+
+n1 = input("Enter name for PERSON1: ")
+b1 = input("Enter birthday for PERSON1 (dd/mm/yyyy)")
+
+n2 = input("Enter name for PERSON2: ")
+b2 = input("Enter birthday for PERSON2 (dd/mm/yyyy)")
+
+n3 = input("Enter name for PERSON3: ")
+b3 = input("Enter birthday for PERSON3 (dd/mm/yyyy)")
+
+n4 = input("Enter name for PERSON4: ")
+b4 = input("Enter birthday for PERSON4 (dd/mm/yyyy)")
+
+n5 = input("Enter name for PERSON5: " )
+b5 = input("Enter birthday for PERSON5 (dd/mm/yyyy)")
+
+#Show registered information
+
+print("Registered information:")
+print("")
+print("   PERSON      NAME      BIRTHDAY (dd/mm/yyyy)")
+print("PERSON1", n1, b1)
+print("PERSON2", n2, b2)
+print("PERSON3", n3, b3)
+print("PERSON4", n4, b4)
+print("PERSON5", n5, b5)
