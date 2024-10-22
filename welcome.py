ec1c2e5d206bc42bca7c3ec05aafe7ce9c4a948d
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-10-21 19:19:29 -0500

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,7 @@
+print("Hello, Welcome to python", end=" ")
+print("I like python", "I learn python", sep="_")
+print(''' This
+            is
+            a
+            multiline
+            print''')
