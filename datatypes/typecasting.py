2b90d29464d3e67350fe96a482e1ee6fb8cce434
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-10-25 18:56:52 -0500

    Add files via upload

---
+++ datatypes/typecasting.py
@@ -0,0 +1,43 @@
+# Float
+a = 4.7
+print("Value of a:", a)
+print("Data-type of a:", type(a))
+
+# Type-casting to INT
+a = int(a)
+print("After typecasting:")
+print("Value of a:", a)
+print("Data-type of a:", type(a))
+
+# Int
+b = 4
+print("Value of b:", b)
+print("Data-type of b:", type(b))
+
+# Type-casting to FLOAT
+b = float(b)
+print("After typecasting:")
+print("Value of b:", b)
+print("Data-type of b:", type(b))
+
+# Boolean
+c = False
+print("Value of c:", c)
+print("Data-type of c:", type(c))
+
+# Type-casting to STR
+c = str(c)
+print("After typecasting:")
+print("Value of c:", c)
+print("Data-type of c:", type(c))
+
+# String
+d = "I am a string. Not very original but I have something to say: Lorem Ipsum dolor sit amet "
+print("Value of d:", d)
+print("Data-type of d:", type(d))
+
+# Type-casting to BOOL
+d = bool(d)
+print("After typecasting:")
+print("Value of d:", d)
+print("Data-type of d:", type(d))
