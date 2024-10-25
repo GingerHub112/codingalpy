26301e30b28fc53366c2f17c2245c8eb8222f19d
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-10-25 18:47:13 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Float
+a = 4.7
+print("Value of a:", a)
+print("Data-type of a:", type(a))
+
+# Int
+b = 4
+print("Value of b:", b)
+print("Data-type of b:", type(b))
+
+# Boolean
+c = False
+print("Value of c:", c)
+print("Data-type of c:", type(c))
+
+# String
+d = "I am a string. Not very original but I have something to say: Lorem Ipsum dolor sit amet "
+print("Value of d:", d)
+print("Data-type of d:", type(d))
