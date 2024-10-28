1649f86b51e45aac117a5c699dbd2137e1e0c9e9
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-10-28 18:45:30 -0500

    Add files via upload

---
+++ operands/main.py
@@ -0,0 +1,13 @@
+# Average Calculator
+print("Welcome to the AVERAGE CALCULATOR")
+
+# Ask values
+av1 = float(input("Enter the 1st result: "))
+av2 = float(input("Enter the 2nd result: "))
+av3 = float(input("Enter the 3rd result: "))
+av4 = float(input("Enter the 4th result: "))
+av5 = float(input("Enter the 5th result: "))
+
+# Algorithm
+print("PROCESSING... IT MIGHT TAKE A WHILE...")
+print((av1 + av2 + av3 + av4 + av5) / 5)
