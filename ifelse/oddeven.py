8482c0627706d2e0153d6552c87d993cdfdb9cda
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-11-01 19:12:33 -0500

    Add files via upload

---
+++ ifelse/oddeven.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number... "))
+
+if (num % 2 == 0):
+    print(num, "is a even number!")
+elif (num % 2 != 0):
+    print(num, '''"I am odd!"''')
+else:
+    print("How did we even get here?!")
