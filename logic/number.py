e45b09a4bf73cbaa277a56712d31a0798946942c
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-11-04 19:01:32 -0500

    Add files via upload

---
+++ logic/number.py
@@ -0,0 +1,13 @@
+a = 5
+b = 2
+c = 7
+
+if (a > b and a > c):
+    print("A is the biggest number")
+else:
+    print("A is NOT the biggest number")
+
+if(a > b or a > c):
+    print("A is NOT the smallest number")
+else:
+    print("A is the smallest number")
