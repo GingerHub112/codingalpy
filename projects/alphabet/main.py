c15143c690fa24a1df37325087088b92531f86e6
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-11-06 11:10:49 -0500

    Add files via upload

---
+++ projects/alphabet/main.py
@@ -0,0 +1,55 @@
+char = "9"
+if (char == "a" ):
+    print("Character is part of the alphabet")
+elif (char == "b" ):
+    print("Character is part of the alphabet")
+elif (char == "c" ):
+    print("Character is part of the alphabet")
+elif (char == "d" ):
+    print("Character is part of the alphabet")
+elif (char == "e" ):
+    print("Character is part of the alphabet")
+elif (char == "f" ):
+    print("Character is part of the alphabet")
+elif (char == "g" ):
+    print("Character is part of the alphabet")
+elif (char == "h" ):
+    print("Character is part of the alphabet")
+elif (char == "i" ):
+    print("Character is part of the alphabet")
+elif (char == "j" ):
+    print("Character is part of the alphabet")
+elif (char == "k" ):
+    print("Character is part of the alphabet")
+elif (char == "l" ):
+    print("Character is part of the alphabet")
+elif (char == "m" ):
+    print("Character is part of the alphabet")
+elif (char == "n" ):
+    print("Character is part of the alphabet")
+elif (char == "o" ):
+    print("Character is part of the alphabet")
+elif (char == "p" ):
+    print("Character is part of the alphabet")
+elif (char == "q" ):
+    print("Character is part of the alphabet")
+elif (char == "r" ):
+    print("Character is part of the alphabet")
+elif (char == "s" ):
+    print("Character is part of the alphabet")
+elif (char == "t" ):
+    print("Character is part of the alphabet")
+elif (char == "u" ):
+    print("Character is part of the alphabet")
+elif (char == "v" ):
+    print("Character is part of the alphabet")
+elif (char == "w" ):
+    print("Character is part of the alphabet")
+elif (char == "x" ):
+    print("Character is part of the alphabet")
+elif (char == "y" ):
+    print("Character is part of the alphabet")
+elif (char == "z" ):
+    print("Character is part of the alphabet")
+else:
+    print("Character is NOT part of the alphabet")
