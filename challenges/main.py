2c2208c01f9935ccf418c4acc835e97a50397212
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-11-11 19:21:14 -0500

    Add files via upload

---
+++ challenges/main.py
@@ -0,0 +1,12 @@
+c1 = 10
+c2 = 20
+c3 = 30
+
+avg = (c1 + c2 + c3) / 3
+
+if (c1 < avg):
+    print("Cyclist 1 is slower than average")
+elif (c2 < avg):
+    print("Cyclist 2 is slower than average")
+else:
+    print("Cyclist 3 is slower than average")
