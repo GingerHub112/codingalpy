317003c2822c9bc2da8b1ceade5de01bbdcaf114
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-12-09 19:14:49 -0500

    Add files via upload

---
+++ turtle/hexagon.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.Screen().bgcolor('yellow')
+turtle.Screen().setup(300,300)
+pen = turtle.Turtle()
+for i in range(6):
+    pen.forward(100)
+    pen.right(60)
---
+++ turtle/star.py
@@ -0,0 +1,9 @@
+import turtle
+pen = turtle.Turtle()
+
+
+for i in range(0, 3):
+    pen.forward(100)
+    pen.left(120)
+
+turtle.done()
