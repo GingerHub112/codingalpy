7826806d239fc6b8ef8195eea723bfd53735c783
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-12-09 19:19:14 -0500

    Add files via upload

---
+++ star.py
@@ -0,0 +1,16 @@
+import turtle
+pen = turtle.Turtle()
+
+
+for i in range(0, 3):
+    pen.forward(100)
+    pen.left(120)
+pen.up()
+pen.left(90)
+pen.forward(50)
+pen.right(90)
+pen.down()
+for i in range(0, 3):
+    pen.forward(100)
+    pen.right(120)
+turtle.done()
