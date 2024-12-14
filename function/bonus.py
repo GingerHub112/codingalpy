b00f56a487ef717d5302664089e84f3015bde695
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-12-13 19:05:42 -0500

    Add files via upload

---
+++ function/bonus.py
@@ -0,0 +1,8 @@
+def intro(name, age, city):
+    print("My name is:", name, "my age is:", age, "and the city where I live is:", city)
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter where you live: ")
+
+intro(name, age, city)
