848941215202e502d4616e5ce3810e68cab366fd
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2024-12-27 09:14:54 -0500

    Add files via upload

---
+++ projects/Shutdown/main.py
@@ -0,0 +1,9 @@
+def shutdown(input1):
+    if input1 == "yes":
+        print("Shutting down")
+    elif input1 == "no":
+        print("Shutdown canceled")
+    else:
+        print("sorry")
+
+shutdown("yes")
