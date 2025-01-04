1d8f6fd18b645f54bfa1f0175f5eb973ea0bb4ff
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2025-01-04 18:11:15 -0500

    Add files via upload

--- projects/amount/main.py
+++ projects/amount/main.py
@@ -2,4 +2,7 @@ x = int(input("What is the total amount of the bill? "))
 y = int(input("How much you paid? "))
 z = x - y
 
-print("You need to pay", z, "more.")
+def calculate():
+    return f"You need to pay {z} more."
+
+print(calculate())
