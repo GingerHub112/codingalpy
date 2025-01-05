9abc3b883f0756dbddf3fa6c0feeb5a2738de9a6
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2025-01-04 19:04:07 -0500

    Add files via upload

---
+++ math/maths.py
@@ -0,0 +1,10 @@
+import math
+
+x = 23.56
+y = 234
+
+print("Floor and Ceilling of 23.56 is", str(math.floor(x)), str(math.ceil(x)))
+print("x copy sign y is", math.copysign(x, y))
+print("SQRT of 23.56 is", math.sqrt(x))
+print("and GCD of y is", int(math.gcd(int(x))))
+print("Cosine and sinus demo", math.cos(x), math.sin(y))
