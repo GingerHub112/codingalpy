261325ad586e6c2a0fb39ae6a0f23667441655fe
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2025-01-21 18:56:34 -0500

    Add files via upload

---
+++ dict/country.py
@@ -0,0 +1,8 @@
+codes = {
+    "India": 91,
+    "Australia": 25,
+    "Nepal": 977
+}
+
+print("Country code for India -", codes.get("India", "Not found"))
+print("Country code for Japan -", codes.get("Japan", "Not found"))
