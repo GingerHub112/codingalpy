2752f148b8a6fdcfabd89ac67acd63f0fd5bb556
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2025-01-25 18:41:58 -0500

    Add files via upload

---
+++ sets/sets.py
@@ -0,0 +1,15 @@
+mySet = {1, 2, 3}
+print(mySet)
+
+mySet = {1.0, "Hello", (1, 2, 3)}
+print(mySet)
+
+mySet = {1, 2, 3, 4, 3, 2}
+print(mySet)
+
+mySet = set([1, 2, 3, 4, 3, 2])
+print(mySet)
+
+mySet = set([0, 1, 3, 4, 5])
+mySet.pop()
+print(mySet)
