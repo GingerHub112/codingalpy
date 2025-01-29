8d1b8695be71a16e8c464fc3684b49eaeccf62e9
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2025-01-28 19:07:06 -0500

    Add files via upload

---
+++ advanced/bonus.py
@@ -0,0 +1,15 @@
+import math
+
+i1 = [1, 3, 5, 7, 9]
+i2 = [2, 4, 6, 8, 10, 11]
+
+zipi = list(zip(i1, i2))
+print(zipi)
+
+lists = [2**3, 3**3, 4**3, 5**3]
+print(list(map(lambda x: math.cbrt(x), lists)))
+
+odd = [i for i in i2 if i%2!=0]
+even = [i for i in i2 if i%2==0]
+print(odd)
+print(even)
