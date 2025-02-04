dfd3b172d95877eae5138a0cb8d8a8d9835262ec
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2025-02-04 17:48:53 -0500

    Update lists.py

--- projects/comprehesion/lists.py
+++ projects/comprehesion/lists.py
@@ -1,6 +1,4 @@
 anwser = int(input("Enter a number: "))
 evens = [x for x in range(anwser) if x % 2 == 0]
 odds = [x for x in range(anwser) if x % 2 != 0]
-print(evens)
-print(odds)
-squares = [x.toUpper() for x in ("apples", "bananas", "oranges")]
+squares = [x.upper() for x in ("apples", "bananas", "oranges")]
