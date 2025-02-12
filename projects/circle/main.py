b6aca274804dd44eca59a3dae925bf1e200485f1
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2025-02-11 19:01:59 -0500

    Add files via upload

--- projects/circle/main.py
+++ projects/circle/main.py
@@ -1,7 +1,14 @@
 import math
 
-def circumeference(diameter):
-    return diameter * math.pi
+class circle:
+    def __init__(self, radius):
+        self.radius = radius
 
-d = int(input("Enter diameter of circle: "))
-print("The circumference of the circle is around:", circumeference(d))
+    def area(self):
+        return math.pi * (self.radius ** 2)
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+my_circle = circle(2.4)
+print("Radius of my circle is {}, the area of my circle is {} and the perimeter of my circle is {}.".format(my_circle.radius, my_circle.area(), my_circle.perimeter()))
